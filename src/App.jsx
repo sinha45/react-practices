4adb4d3e59584fdb434c63ff027e4e091dd0bfa9
sinha45 <alfa45leonidas@gmail.com>
2024-11-23 15:22:20 +0600

    counter app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div>
+
+        <button onClick={() => setCount(count + 1)}>++</button><h3>{count}</h3><button onClick={() => setCount(count - 1)}>--</button>
+      </div>
+      <button className="reset" onClick={() => setCount(0)}>Reset</button>
+    </>
+  )
+}
+
+export default App
